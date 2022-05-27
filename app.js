15aedcc9a2eedc914c328ff851419cc243d4ccac
RazorHollow <rob@razorhollow.com>
2022-05-27 09:19:27 -0400

    define getGuess method

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  },
+  getGuess: function() {
+    return parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
   }
 }
