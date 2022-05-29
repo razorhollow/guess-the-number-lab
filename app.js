4eeb577f930962ff4b2667f11bede3c202b749ca
RazorHollow <rob@razorhollow.com>
2022-05-29 09:32:37 -0400

    complete level-ups

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
-  smallestNum: 1,
+  biggestNum: null,
+  smallestNum: null,
   secretNum: null,
   prevGuesses: [],
   play: function() {
+    this.setRange()
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     let guess
     do {
@@ -34,19 +35,16 @@ const game = {
       alert(`Congrats! You guessed the number in ${arr.length + 1}!`)
     } else if (guess > this.secretNum){
       this.prevGuesses.push(guess)
+      this.biggestNum = guess
       alert(`Your guess is too high. Previous guesses: ${arr.join(", ")}`)
     } else if (guess < this.secretNum){
       this.prevGuesses.push(guess)
+      this.smallestNum = guess
       alert(`Your guess is too low. Previous guesses: ${arr.join(", ")}`)
     }
+  },
+  setRange: function(){
+    this.biggestNum = parseInt(prompt(`Enter Largest Possible Number`))
+    this.smallestNum = parseInt(prompt(`Enter Smallest Possible Number`))
   }
 } 
-
-
-// ## Level Up 🚀
-
-// - If the player enters a number greater than the `secretNum` make it the new `biggestNum`, so that the player can't enter a number greater than it. If the player enters a number that is less than the `secretNum` make it the new `smallestNum`, so that the player can't enter a number less than it. Hint: You may want to add a helper function to accomplish this.
-
-// ## More Level Up 🚀
-
-// When `play` is run, immediately prompt the player to enter the smallest and biggest numbers instead of pre-setting values.
