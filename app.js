9c1fd283d622afbf1ce2bf869899d89052a68ae9
RazorHollow <rob@razorhollow.com>
2022-05-29 08:29:05 -0400

    complete task 4

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const game = {
   secretNum: null,
   prevGuesses: [],
   play: function() {
-    this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  },
+    this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    let guess
+    do {
+      guess = this.getGuess()
+      console.log(guess)
+    }while (guess !== this.secretNum) 
+    
+  }
+
+  ,
   getGuess: function() {
     let currentGuess
     let input = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
@@ -20,5 +27,26 @@ const game = {
         }
       }
      return currentGuess
-    } 
+    }
+  ,
+  render: function() {
+
+  }
   } 
+
+// 5. Add a `render` method to `game` that `play` will call after a guess has been made that alerts:
+//   - If the secret has been guessed: `Congrats! You guessed the number in [number of prevGuesses]!`
+//   - Otherwise: `Your guess is too [high|low] Previous guesses: x, x, x, x`
+//   - Hints:
+//       - `render` won’t be able to access any of `play`’s local variables, e.g., `guess`, so be sure pass `render` any arguments as needed (you may not have built your program to use any, that's ok if that's the case!)
+//       - Template literals not only have interpolation, but they also honor whitespace - including line breaks!
+//       - The list of previous guesses can be generated using the array `join` method.
+// 6. The `play` method should end (`return`) when the guess matches `secretNum`.
+
+// ## Level Up 🚀
+
+// - If the player enters a number greater than the `secretNum` make it the new `biggestNum`, so that the player can't enter a number greater than it. If the player enters a number that is less than the `secretNum` make it the new `smallestNum`, so that the player can't enter a number less than it. Hint: You may want to add a helper function to accomplish this.
+
+// ## More Level Up 🚀
+
+// When `play` is run, immediately prompt the player to enter the smallest and biggest numbers instead of pre-setting values.
